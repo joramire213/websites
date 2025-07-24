2959a73da6f72ec025e7e3e98a9cb5b515b45ade
Jorge Ramirez <ar917671@gmail.com>
2025-07-24 17:38:07 -0500

    burger salvation

--- test-screen/recursos/js/script.js
+++ test-screen/recursos/js/script.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
     const navLinks = document.querySelector('.nav-links');
-    const body = document.querySelector('body');
 
     if (navToggle && navLinks) {
-        navToggle.addEventListener('click', () => {
-            // Activa/desactiva el menú
+        navToggle.addEventListener('click', function() {
             navLinks.classList.toggle('nav-active');
-            
-            // Opcional pero recomendado: Evita que se haga scroll en la página de fondo cuando el menú está abierto
-            body.classList.toggle('no-scroll');
         });
     }
 });
