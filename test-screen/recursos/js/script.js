5e13b907a02f1cb2f720c85a18b24e4df33de519
Jorge Ramirez <ar917671@gmail.com>
2025-07-24 17:33:57 -0500

    burger master

--- test-screen/recursos/js/script.js
+++ test-screen/recursos/js/script.js
@@ -1,22 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
     const navLinks = document.querySelector('.nav-links');
-    const header = document.querySelector('.page-header');
+    const body = document.querySelector('body');
 
-    if (navToggle && navLinks && header) {
-        const setMenuPosition = () => {
-            const headerHeight = header.offsetHeight;
-            navLinks.style.top = `${headerHeight}px`;
-            navLinks.style.height = `calc(100vh - ${headerHeight}px)`;
-        };
-
-        // Ajusta la posición al cargar y al cambiar el tamaño de la ventana
-        setMenuPosition();
-        window.addEventListener('resize', setMenuPosition);
-
-        // Maneja el clic del botón
+    if (navToggle && navLinks) {
         navToggle.addEventListener('click', () => {
+            // Activa/desactiva el menú
             navLinks.classList.toggle('nav-active');
+            
+            // Opcional pero recomendado: Evita que se haga scroll en la página de fondo cuando el menú está abierto
+            body.classList.toggle('no-scroll');
         });
     }
 });
